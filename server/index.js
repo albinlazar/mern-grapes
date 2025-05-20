bfae88615be16432e29c7bd4c64878219bb27ef2
albin <albin@gmail.com>
2025-05-20 14:46:15 +0530

    updated index

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
-import express from 'express'
+import express from "express";
 
 const app= express();
 const port = 3000;
+app.get("/test",(req,res)=>{
+    res.send("success");
+});
 app.listen(port,(req,res)=>{
     console.log(`server is running on port ${port}`);
-})
+});
